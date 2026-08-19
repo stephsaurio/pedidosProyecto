ece9c7cf3933478151e1db6f8c404621163f92e9
emelyemunoz <emelyemunoz2029@gmail.com>
2026-08-18 20:01:21 -0600

    Agregar dependencias y rutas de productos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Registro } from './clientes/registro/registro';
 import { Login } from './clientes/login/login';
+import { Catalogo } from './producto/catalogo/catalogo';
 
 export const routes: Routes = [
-    { path: 'registro', component: Registro },
-    { path: 'login', component: Login },
-    { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: 'registro', component: Registro },
+  { path: 'login', component: Login },
+  { path: 'catalogo', component: Catalogo },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
